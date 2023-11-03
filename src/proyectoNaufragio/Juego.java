6e4ac81e85659d200868c386ecc462d3d3c7d64b
Génesis Balcazar <Gnesis Balcazar@LAPTOP-K1JOBEN3>
2023-11-03 17:10:06 +0100

    Añadir nuevas clases

---
+++ src/proyectoNaufragio/Juego.java
@@ -0,0 +1,109 @@
+package proyectoNaufragio;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+// JUEGO DE PRUEBA
+// MATRIZ 9X9
+/**
+ * El juego tendra un JFrame interactivo con un gridLayout de 9x9.
+ * El tablero de juego será una matriz de 9x9 y en cada hueco habrá un boton que cuando el jugador acierte que hay un barco,
+ * la casilla se pondrá azul, si no se pondrá rojo. Los barcos se colocarán siempre en el mismo sitio.
+ * Hay 3 barcos de diferente longitud: barco1 longitud de 4 casillas, barco2 longitud de 2 y barco3 de longitud de 5.
+ * Estos solo pueden estar colocados en vertical o en horizontal.
+ * Importante que no sea en diagonal, y no se pueden sobre poner entre ellos.
+ * Cada vez que se haga clic en una casilla/botón habrá un contador de clicks.
+ * Si el ususario consigue destapar una casilla azul sale una notificación de "¡Tocado!"
+ * y que barco ha destapado y de que longitud es. Y cuando destape todos los barcos saldra una notificación de fin del juego y
+ * cuantos clicks ha dado. Donde colocar los barcos: - Barco1: [1][0], [1][1],[1][2], [1][3] - Barco2: [6][7],[6][8] - Barco3: [2][1],[3][1],[4][1],[5][1],[6][1
+ *
+ *
+ */
+public class Juego {
+    private Casilla[][] tablero;
+    private int contadorClicks;
+    private JFrame frame;
+
+    public Juego() {
+        tablero = new Casilla[9][9];
+        contadorClicks = 0;
+        frame = new JFrame("Hundir la flota");
+        frame.setLayout(new GridLayout(9,9));
+        inicializarTablero();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    private void inicializarTablero() {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                tablero[i][j] = new Casilla(i, j);
+                // Coloca los barcos en las posiciones deseadas
+                if ((i == 1 && j >= 0 && j <= 3) || (i == 6 && j >= 7 && j <= 8) || (i >= 2 && i <= 6 && j == 1)) {
+                    tablero[i][j].setHayBarco(true);
+                }
+                frame.add(tablero[i][j]);
+            }
+        }
+    }
+
+    public int getContadorClicks() {
+        return contadorClicks;
+    }
+
+    class Casilla extends JButton {
+        private boolean hayBarco;
+        private int x, y;
+
+        public Casilla(int x, int y) {
+            this.x = x;
+            this.y = y;
+            this.hayBarco = false;
+            this.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    clickCasilla();
+                }
+            });
+        }
+
+        public boolean isHayBarco() {
+            return hayBarco;
+        }
+
+        public void setHayBarco(boolean hayBarco) {
+            this.hayBarco = hayBarco;
+        }
+
+        public void clickCasilla() {
+            contadorClicks++;
+            if (hayBarco) {
+                setBackground(Color.BLUE);
+                JOptionPane.showMessageDialog(frame, "¡Tocado!");
+                verificarFinDelJuego();
+            } else {
+                setBackground(Color.RED);
+            }
+        }
+
+        private void verificarFinDelJuego() {
+            for (int i = 0; i < 9; i++) {
+                for (int j = 0; j < 9; j++) {
+                    if (tablero[i][j].isHayBarco() && tablero[i][j].getBackground() != Color.BLUE) {
+                        return;
+                    }
+                }
+            }
+            JOptionPane.showMessageDialog(frame, "¡Hundido! Has dado " + contadorClicks + " clicks.");
+        }
+    }
+
+    public static void main(String[] args) {
+        new Juego();
+    }
+}
+
+
---
+++ src/proyectoNaufragio/PantallaPerfil.java
@@ -0,0 +1,114 @@
+package proyectoNaufragio;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class PantallaPerfil extends JFrame {
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public PantallaPerfil() {
+        this.setVisible(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(600, 400);
+        this.setTitle("Perfil");
+
+        // Colores
+        Color Rosita = new Color(255, 102, 196);
+        Color RositaClarito = new Color(255, 128, 234);
+
+        this.setLayout(new BorderLayout());
+        JPanel pantNorte = new JPanel();
+        JPanel pantSur = new JPanel();
+        JPanel pantEste = new JPanel();
+        JPanel pantOeste = new JPanel();
+        JPanel pantCentro = new JPanel();
+        pantCentro.setLayout(new GridLayout(9, 9));
+        pantEste.setLayout(new GridLayout(3, 1));
+
+        // NORTE
+        JLabel titUs = new JLabel("Usuario");
+        titUs.setForeground(RositaClarito);
+        titUs.setFont(new Font("Arial", Font.BOLD, 40));
+        pantNorte.add(titUs);
+
+        // CENTRO
+        JTextField usuario = new JTextField("Nickname: ");
+        usuario.setBackground(Rosita);
+        pantCentro.add(usuario);
+
+        JTextField mail = new JTextField("EMail: ");
+        mail.setBackground(Rosita);
+        pantCentro.add(mail);
+
+        JTextField partidas = new JTextField("Partidas totales: ");
+        partidas.setBackground(Rosita);
+        pantCentro.add(partidas);
+        // Falta añadir el icono o perfil de usuario
+
+        // OESTE
+        pantOeste.setLayout(new GridLayout(9, 9));
+
+        // ESTE
+        JButton bSonido = new JButton("Sonido");
+        JButton bMusica = new JButton("Música");
+        JButton bInfo = new JButton("Información");
+        bInfo.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, "BIENVENIDO A NAUFRAGIO EN EL PACÍFICO\r\n"
+                        + "\r\n"
+                        + "* Info general\r\n"
+                        + " - Descripción\r\n"
+                        + "* Reglas de juego\r\n"
+                        + " - Explicación de cómo funciona el juego\r\n"
+                        + "* Contacto\r\n"
+                        + " - Email\r\n"
+                        + " - Tel.\r\n"
+                        + " - Redes sociales");
+            }
+        });
+        pantEste.add(bInfo);
+        pantEste.add(bMusica);
+        pantEste.add(bSonido);
+        
+     // SUR
+        JButton bJugar = new JButton("Modo Juego");
+        bJugar.setBackground(Rosita);
+        bJugar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Código para ir a la clase "pantallaModoJuego"
+                new PantallaModoJuego();
+                dispose(); // Cierra ventana
+            }
+        });
+        pantSur.add(bJugar);
+
+        this.add(pantNorte, BorderLayout.NORTH);
+        this.add(pantSur, BorderLayout.SOUTH);
+        this.add(pantCentro, BorderLayout.CENTER);
+        this.add(pantEste, BorderLayout.EAST);
+        this.add(pantOeste, BorderLayout.WEST);
+        this.setVisible(true);
+    }
+
+    public static void main(String args[]) {
+        new PantallaPerfil();
+    }
+}
+
