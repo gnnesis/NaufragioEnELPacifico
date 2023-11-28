f6dddb6de39ce8c57de43bc8095f4324e017cc53
Izaro Fuzhi Serrano Otaduy <izarofserrano@gmail.com>
2023-11-28 18:39:04 +0100

    Se incluyen test preliminares de las clases Casilla y Usuario.

--- src/test/UsuarioTest.java
+++ src/test/UsuarioTest.java
@@ -2,6 +2,13 @@ package test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -13,6 +20,7 @@ class UsuarioTest {
 	private static final String NICK = "test";
 	private static final String PASS = "test";
 	private static final String CODED_PASS = "098f6bcd4621d373cade4e832627b4f6";
+	private static final String FILE_PATH = "ruta/del/archivo.txt";
 	
 	@BeforeAll
 	static void init()
@@ -22,8 +30,10 @@ class UsuarioTest {
 
 	@Test
 	void testUsuario() {
-		fail("Not yet implemented");
-	}
+		 assertNotNull(u);
+		 assertEquals(NICK, u.getNickname());
+		 assertEquals(PASS, u.getContrasena());
+		}
 
 	@Test
 	void testGetNickname() {
@@ -53,12 +63,34 @@ class UsuarioTest {
 
 	@Test
 	void testComprobarContrasena() {
-		fail("Por hacer aun");
+		 assertTrue(u.comprobarContrasena(PASS));
+	     assertFalse(u.comprobarContrasena("contrasenaIncorrecta"));
 	}
 
 	@Test
 	void testRegistrar() {
-		fail("Not yet implemented");
+		
+    }
+
+    
+
+    @Test
+    void testGetMd5() {
+        // Prueba con una cadena simple
+        String input = "password123";
+        String expectedHash = "482c811da5d5b4bc6d497ffa98491e38";
+        assertEquals(expectedHash, u.getMd5(input));
+
+        // Prueba con una cadena vacía
+        input = "";
+        expectedHash = "d41d8cd98f00b204e9800998ecf8427e";
+        assertEquals(expectedHash, u.getMd5(input));
+
+        // Prueba con una cadena más larga y compleja
+        input = "estaEsUnaCadenaLargaYCompleja123!@#";
+        expectedHash = "1e8324765e0b5506fde94626b8a9d7c1";
+        assertEquals(expectedHash, u.getMd5(input));
+        
 	}
 
 }
