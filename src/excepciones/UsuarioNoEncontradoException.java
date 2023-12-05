901582ec077d54a46d468a038490afa8c09721ba
gnnesis <g.balcazar@opendeusto.es>
2023-12-05 14:19:37 +0100

    Se incluye una nueva excepción en el paquete de excepciones

---
+++ src/excepciones/UsuarioNoEncontradoException.java
@@ -0,0 +1,9 @@
+package excepciones;
+
+@SuppressWarnings("serial")
+public class UsuarioNoEncontradoException extends Exception {
+	
+	public UsuarioNoEncontradoException() {
+		super("El usuario introducido no ha sido encontrado");
+	}
+}
